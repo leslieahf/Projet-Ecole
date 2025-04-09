aeaafd8e3ff4da9476b928dddb44dd15f8e37392
LuXiN42 <loi.rob03@gmail.com>
2025-04-09 18:37:03 +0200

    page profil autres page visualisation

--- resources/views/profilautres.blade.php
+++ resources/views/profilautres.blade.php
@@ -1,34 +1,46 @@
 @extends('layout')
+
+@section('head') <!-- Section pour remplir le head du layout -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil des autres utilisateurs - IntelliSchool</title>
+    <link href="{{ asset('css/profilautres.css') }}" rel="stylesheet"> <!-- Lien vers le fichier CSS spécifique -->
+@endsection
+
 @section('contenu')
 <div class="main-content">
 
-<!-- Page Content -->
-<section class="py-16 px-6 bg-white">
-<p class="text-lg text-gray-700 mb-8">Voici le profil des autres membres :</p>
-<table>
-    <tr>
-        <th>Photo</th>
-        <th>Login</th>
-        <th>Age</th>
-        <th>Sexe</th>
-        <th>Date de naissance</th>
-        <th>Type de membre</th>
-    </tr>
-@foreach($utilisateurs as $utilisateur)
-    @if ($utilisateur->login)
-        <tr>
-            <td>
-            @if($utilisateur->photo)
-                <img src="{{ Storage::url($utilisateur->photo) }}" width="100" alt="Photo de profil">
-            @endif
-            </td>
-            <td>{{ $utilisateur->login }}</td>
-            <td>{{ $utilisateur->age }}</td>
-            <td>{{ $utilisateur->sexe }}</td>
-            <td>{{ $utilisateur->date_de_naissance }}</td>
-            <td>{{ $utilisateur->type_membre }}</td>
-        </tr>
-    @endif
-@endforeach
-</table>
+    <!-- Page Content -->
+    <section class="py-16 px-6 bg-white">
+        <p class="text-lg text-gray-700 mb-8">Voici le profil des autres membres :</p>
+
+        <table>
+            <tr>
+                <th>Photo</th>
+                <th>Login</th>
+                <th>Age</th>
+                <th>Sexe</th>
+                <th>Date de naissance</th>
+                <th>Type de membre</th>
+            </tr>
+
+            @foreach($utilisateurs as $utilisateur)
+                @if ($utilisateur->login)
+                    <tr>
+                        <td>
+                            @if($utilisateur->photo)
+                                <img src="{{ Storage::url($utilisateur->photo) }}" width="100" alt="Photo de profil">
+                            @endif
+                        </td>
+                        <td>{{ $utilisateur->login }}</td>
+                        <td>{{ $utilisateur->age }}</td>
+                        <td>{{ $utilisateur->sexe }}</td>
+                        <td>{{ $utilisateur->date_de_naissance }}</td>
+                        <td>{{ $utilisateur->type_membre }}</td>
+                    </tr>
+                @endif
+            @endforeach
+        </table>
+    </section>
+</div>
 @endsection
